8a5cfb1a2ea4d06529594a2d081d77bacc8fbf81
weixsong <watkinsong@163.com>
2016-08-29 19:19:04 +0800

    update

--- src/mnist_average_darkness.py
+++ src/mnist_average_darkness.py
@@ -26,13 +26,16 @@ import mnist_loader
 
 def main():
     training_data, validation_data, test_data = mnist_loader.load_data()
+
     # training phase: compute the average darknesses for each digit,
     # based on the training data
     avgs = avg_darknesses(training_data)
+
     # testing phase: see how many of the test images are classified
     # correctly
     num_correct = sum(int(guess_digit(image, avgs) == digit)
                       for image, digit in zip(test_data[0], test_data[1]))
+
     print "Baseline classifier using average darkness of image."
     print "%s of %s values correct." % (num_correct, len(test_data[1]))
 
@@ -43,12 +46,15 @@ def avg_darknesses(training_data):
     particular image is just the sum of the darknesses for each pixel."""
     digit_counts = defaultdict(int)
     darknesses = defaultdict(float)
+
     for image, digit in zip(training_data[0], training_data[1]):
         digit_counts[digit] += 1
         darknesses[digit] += sum(image)
+
     avgs = defaultdict(float)
     for digit, n in digit_counts.iteritems():
         avgs[digit] = darknesses[digit] / n
+
     return avgs
 
 def guess_digit(image, avgs):
@@ -58,7 +64,9 @@ def guess_digit(image, avgs):
     are the corresponding average darknesses across the training data."""
     darkness = sum(image)
     distances = {k: abs(v-darkness) for k, v in avgs.iteritems()}
+
     return min(distances, key=distances.get)
+
 
 if __name__ == "__main__":
     main()
